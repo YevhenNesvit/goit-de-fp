22ff2b23bb55b7981bb74cca6ad92483a5bac604
Yevhen Nesvit <yevhen.nesvit@gmail.com>
2024-12-10 12:01:03 +0200

    initial commit

---
+++ first_part/configs.py
@@ -0,0 +1,11 @@
+kafka_config = {
+    "bootstrap_servers": '77.81.230.104:9092',
+    "username": 'admin',
+    "password": 'VawEzo1ikLtrA8Ug8THa',
+    "security_protocol": 'SASL_PLAINTEXT',
+    "sasl_mechanism": 'PLAIN'
+}
+
+jdbc_url = "jdbc:mysql://217.61.57.46:3306/olympic_dataset"
+jdbc_user = "neo_data_admin"
+jdbc_password = "Proyahaxuqithab9oplp"
