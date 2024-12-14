86a5c05de229ec725b1c8aded2e5451d52a72b73
Yevhen Nesvit <yevhen.nesvit@gmail.com>
2024-12-14 17:39:19 +0200

    add task-2 solution

---
+++ second_part/landing_to_bronze.py
@@ -0,0 +1,34 @@
+import requests
+from pyspark.sql import SparkSession
+
+def download_data(local_file_path):
+    url = "https://ftp.goit.study/neoversity/"
+    downloading_url = url + local_file_path + ".csv"
+    print(f"Downloading from {downloading_url}")
+    response = requests.get(downloading_url)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Open the local file in write-binary mode and write the content of the response to it
+        with open(local_file_path + ".csv", 'wb') as file:
+            file.write(response.content)
+        print(f"File downloaded successfully and saved as {local_file_path}")
+    else:
+        exit(f"Failed to download the file. Status code: {response.status_code}")
+
+def save_to_bronze(table_name):
+    spark = SparkSession.builder.appName("LandingToBronze").getOrCreate()
+    
+    # Read CSV file
+    df = spark.read.csv(f"{table_name}.csv", header=True, inferSchema=True)
+    
+    # Save as Parquet
+    output_path = f"second_part/bronze/{table_name}"
+    df.write.mode("overwrite").parquet(output_path)
+    print(f"Data saved to {output_path}")
+    df.show()
+
+tables = ["athlete_bio", "athlete_event_results"]
+for table in tables:
+    download_data(table)
+    save_to_bronze(table)
---
+++ second_part/project_solution.py
@@ -0,0 +1,39 @@
+from airflow import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from datetime import datetime
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 12, 1),
+    'retries': 1,
+}
+
+dag = DAG(
+    dag_id="batch_data_lake",
+    default_args=default_args,
+    schedule=None,
+    catchup=False,
+)
+
+# Tasks
+landing_to_bronze = SparkSubmitOperator(
+    task_id="landing_to_bronze",
+    application="/second_part/landing_to_bronze.py",
+    dag=dag,
+)
+
+bronze_to_silver = SparkSubmitOperator(
+    task_id="bronze_to_silver",
+    application="second_part/bronze_to_silver.py",
+    dag=dag,
+)
+
+silver_to_gold = SparkSubmitOperator(
+    task_id="silver_to_gold",
+    application="second_part/silver_to_gold.py",
+    dag=dag,
+)
+
+# Task Dependencies
+landing_to_bronze >> bronze_to_silver >> silver_to_gold
